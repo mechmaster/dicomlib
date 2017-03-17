feefa39fa0aac024aeb5e5a9523fd5eafbd5ae1c
Vladimir Gavrilin <vladimir-serd@yandex.ru>
2017-03-17 11:59:35 +0400

    Refactoring AssociationRejection

--- lib/AssociationRejection.cpp
+++ lib/AssociationRejection.cpp
@@ -1,82 +1,79 @@
 #include "AssociationRejection.hpp"
+
 namespace dicom
 {
+  /*
+  exception class.
+  */
 
+  AssociationRejection::AssociationRejection(BYTE Result, BYTE Source, BYTE Reason) :
+    dicom::exception("Association Rejection"),
+    m_result(Result),
+    m_source(Source),
+    m_reason(Reason)
+  {
+  }
 
-	/*
-		exception class.
-	*/
+  std::string AssociationRejection::GetResult() const 
+  {
+    switch (m_result)
+    {
+      case 0x01:
+        return "Rejected Permanent";
+      case 0x02:
+        return "Rejected Transient";
+      default:
+        return "Undefined";
+    }
+  }
 
-	AssociationRejection::AssociationRejection(BYTE Result, BYTE Source, BYTE Reason)
-	:
-		dicom::exception("Association Rejection"),
-		Result_(Result),
-		Source_(Source),
-		Reason_(Reason)
-	{
+  std::string AssociationRejection::GetSource() const
+  {
+    switch (m_source)
+    {
+      case 0x01:
+        return "DICOM UL service-user";
+      case 0x02:
+        return "DICOM UL service-provider (ACSE related function)";
+      case 0x03:
+        return "DICOM UL service-provider (Presentation related function)";
+      default:
+        return "Undefined";
+    }
+  }
 
-	}
-	std::string AssociationRejection::GetResult() const 
-	{
-		switch (Result_)
-		{
-		case 0x01:
-			return "Rejected Permanent";
-		case 0x02:
-			return "Rejected Transient";
-		default:
-			return "Undefined";
-		}
-	}
-	std::string AssociationRejection::GetSource() const
-	{
-		switch(Source_)
-		{
-		case 0x01:
-			return "DICOM UL service-user";
-		case 0x02:
-      		return "DICOM UL service-provider (ACSE related function)";
-		case 0x03:
-      		return "DICOM UL service-provider (Presentation related function)";
-		default:
-			return "Undefined";
-		}
-	}
+  std::string AssociationRejection::GetReason() const
+  {
+    switch (m_source)
+    {
+      case 0x01:
+        switch (m_reason)
+        {
+          case 0x01:
+            return "No reason given.";
+          case 0x02:
+            return "Application Context not supported.";
+          case 0x03:
+            return "Calling AE not recognized";
+          case 0x07:
+            return "Protocol version not supported";
+        }
+      case 0x02:
+        switch(m_reason)
+        {
+          case 0x02:
+          return "Protocol Version Not Supported.";
+        }
+      case 0x03:
+        switch (m_reason)
+        {
+          case 0x01:
+            return ("Temporary Congestion");
+          case 0x02:
+            return ("Local Limit Exceeded.");
+        }
+    }
 
-
-	std::string AssociationRejection::GetReason() const
-	{
-		switch(Source_)
-		{
-		case 0x01:
-			switch(Reason_)
-			{
-				case 0x01:
-					return "No reason given.";
-				case 0x02:
-					return "Application Context not supported.";
-				case 0x03:
-					return "Calling AE not recognized";
-				case 0x07:
-					return "Protocol version not supported";
-			}
-		case 0x02:
-			switch(Reason_)
-			{
-				case 0x02:
-					return "Protocol Version Not Supported.";
-			}
-
-		case 0x03:
-			switch(Reason_)
-			{
-			case 0x01:
-				return ("Temporary Congestion");
-			case 0x02:
-				return ("Local Limit Exceeded.");
-			}
-		}
-
-		return "Unknown reason";
-	}
+    return "Unknown reason";
+  }
 }//namespace dicom
