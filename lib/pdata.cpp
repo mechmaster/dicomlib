809c9eeb21701a50d9954eaaaa5fac7472112a48
mechmaster <vladimir-serd@yadex.ru>
2017-10-15 17:20:49 +0400

    Refactoring pdata

--- lib/pdata.cpp
+++ lib/pdata.cpp
@@ -1,129 +1,131 @@
-#include "pdata.hpp"
 #include <iostream>
 #include <assert.h>
+
 #include "Decoder.hpp"
 #include "iso646.h"
-//#include "Profiling.hpp"
 
-
-
+#include "pdata.hpp"
 
 /*
-	This is seriously ugly, and quite misleading.  We definitely need some clean up work here!
-
+This is seriously ugly, and quite misleading.  We definitely need some clean up work here!
 */
+
 namespace dicom
 {
 
-	/*
-		I think the object model presented in this file
-		is sub-optimal.
-	*/
+/*
+I think the object model presented in this file
+is sub-optimal.
+*/
 
-	/*!
-		defined in Part8, table 9-23
-	*/
-	class	PDV//presentation data value item
-	{
-	public:
-		UINT32	Length;
-		BYTE	PresentationContextID;
-		BYTE	MessageHeader;
-		//defined in Part 8, Annex E.2, see figure E.2-1
+/*!
+defined in Part8, table 9-23
+*/
+class PDV//presentation data value item
+{
 
-		bool IsCmdMessage(){return (MessageHeader bitand MessageControlHeader::COMMAND);}
-		bool IsDataMessage(){return not(MessageHeader bitand MessageControlHeader::COMMAND);}
-		bool IsLastFragment(){return ((MessageHeader bitand MessageControlHeader::LAST_FRAGMENT)!=0);}
-		//UINT32 ReadDynamic(Network::Socket& socket);
-	};
+public:
 
-	/*!
-		documented in Part 8, section 7.6,  figure  9-2 and tables 9-22 and 9-23
-	*/
+  std::uint32_t m_length;
+  std::uint8_t m_presentationContextID;
+  std::uint8_t m_messageHeader;
 
- 	bool	PDataTF::ReadDynamic(Network::Socket& socket)
- 	{
-//		scoped_profiler prof("PDataTF::ReadDynamic") ;
- 		UINT32		Count;
+//defined in Part 8, Annex E.2, see figure E.2-1
 
- 		if(!Length)//why would this ever not be the case?
- 		{
-			BYTE Reserved_dummy;
- 			socket >> Reserved_dummy;//shouldn't this fail?
- 			socket >> Length;
- 		}
+  bool IsCmdMessage()
+  {
+    return (m_messageHeader & MessageControlHeader::COMMAND);
+  }
 
- 		Count = Length;
- 		MsgStatus = 0;	// continue
-		PDV pdv;//should be inside following loop, i think
- 		while ( Count > 0)
- 		{
+  bool IsDataMessage()
+  {
+    return !(m_messageHeader & MessageControlHeader::COMMAND);
+  }
 
- 			//I think that this should happen in a member of PDV-.
- 			socket >> pdv.Length;
- 			socket >> pdv.PresentationContextID;
- 			socket >> pdv.MessageHeader;
+  bool IsLastFragment()
+  {
+    return ((m_messageHeader & MessageControlHeader::LAST_FRAGMENT) != 0);
+  }
+};
 
+/*!
+documented in Part 8, section 7.6,  figure  9-2 and tables 9-22 and 9-23
+*/
 
-			//now read actual data from socket onto buffer.
-			//std::vector<BYTE> data(pdv.Length-2);
-			//socket >> data;
-			//std::copy(data.begin(),data.end(),std::back_inserter(buffer_));
-			buffer_.insert(buffer_.end(),pdv.Length-2,0x00);
-			socket.Readn(&*(buffer_.end()-(pdv.Length-2)),(pdv.Length-2));//slightly more complicated.
+bool PDataTF::read(Buffer& temp)
+{
+  std::uint32_t count;
 
+  if(!m_length)//why would this ever not be the case?
+  {
+    std::uint8_t reservedDummy;
+    temp >> reservedDummy;//shouldn't this fail?
+    temp >> m_length;
+  }
 
-			/*
-				The previous 3 lines could be dramatically speeded up if
-				we _first_ allocate the space on buffer, then pass _pointers_
-				to the begin and end of the new space to Socket::Readn();
+  count = m_length;
+  m_msgStatus = 0; // continue
+  PDV pdv;//should be inside following loop, i think
 
-				I'm not going to change this till I have a good unit test for the change
+  while (count > 0)
+  {
+    //I think that this should happen in a member of PDV-.
+    temp >> pdv.m_length;
+    temp >> pdv.m_presentationContextID;
+    temp >> pdv.m_messageHeader;
 
-				Meyers says in 'Efficient STL' that member functions are preferable
-				over algorithms for efficiency?
+    //now read actual data from socket onto buffer.
 
-				So possible optimizations would be:
-				1)
+    temp >> m_buffer;
 
-				buffer_.insert(buffer_.end(),data.begin(),data.end());//this is simple.
+    /*
+    The previous 3 lines could be dramatically speeded up if
+    we _first_ allocate the space on buffer, then pass _pointers_
+    to the begin and end of the new space to Socket::Readn();
 
-				2)
+    I'm not going to change this till I have a good unit test for the change
 
-				buffer_.insert(buffer_.end(),pdv.Length-2,0x00);
-				socket.Readn(buffer_.end()-(pdv.Length-2),(pdv.Length-2));//slightly more complicated.
+    Meyers says in 'Efficient STL' that member functions are preferable
+    over algorithms for efficiency?
 
-				We need profiling tests for both, such as dicomtest::SubmitLotsOfImages()
-			*/
+    So possible optimizations would be:
+    1)
 
- 			Count = Count - pdv.Length - sizeof(UINT32);
- 			Length = Length - pdv.Length - sizeof(UINT32);
+    buffer_.insert(buffer_.end(),data.begin(),data.end());//this is simple.
 
-			if(pdv.IsLastFragment())
- 			{
- 				MsgStatus = 1;
- 				PresentationContextID = pdv.PresentationContextID;
- 				return ( true );
- 			}
- 		}
+    2)
 
-		if(pdv.IsLastFragment())
+    buffer_.insert(buffer_.end(),pdv.Length-2,0x00);
+    socket.Readn(buffer_.end()-(pdv.Length-2),(pdv.Length-2));//slightly more complicated.
 
-		{
-			assert(0);//how can this ever happen?
-			MsgStatus = 1;
-		}
+    We need profiling tests for both, such as dicomtest::SubmitLotsOfImages()
+    */
 
- 		PresentationContextID = pdv.PresentationContextID;
+    count = count - pdv.m_length - sizeof(std::uint32_t);
+    m_length = m_length - pdv.m_length - sizeof(std::uint32_t);
 
- 		return ( true );
- 	}
+    if (pdv.IsLastFragment())
+    {
+      m_msgStatus = 1;
+      m_presentationContextID = pdv.m_presentationContextID;
+      return true;
+    }
+  }
 
-	PDataTF::PDataTF(int ByteOrder)
-	:buffer_(ByteOrder)
-	,Length(0)
+  if(pdv.IsLastFragment())
+  {
+    assert(0);//how can this ever happen?
+    m_msgStatus = 1;
+  }
 
-	{
-	}
+  m_presentationContextID = pdv.m_presentationContextID;
 
+  return true;
+}
+
+PDataTF::PDataTF(int ByteOrder) :
+  m_buffer(ByteOrder),
+  m_length(0)
+{
+}
 }//namespace dicom
--- lib/pdata.hpp
+++ lib/pdata.hpp
@@ -1,5 +1,3 @@
-#ifndef P_DATA_HPP_INCLUDE_GUARD_4520084566
-#define P_DATA_HPP_INCLUDE_GUARD_4520084566
 /****************************************************************************
  *
  * Abstract Base Class for P-DATA Service
@@ -7,63 +5,61 @@
  *
  ****************************************************************************/
 
+#pragma once
+
 #include "Types.hpp"
 #include "DataSet.hpp"
+#include "Buffer.hpp"
 
 /*
-
-	We could redesign the following two classes to mirror the standard better
-	by having PDataTF HAVE A list<> of PDVs.  I think we can avoid copying-related
-	performance issues if this is THE place where the associated DataSets reside, and
-	they are passed to other classes by reference.  After all, I don't see why a
-	Command or Data message would ever be changed after it had been received.
+We could redesign the following two classes to mirror the standard better
+by having PDataTF HAVE A list<> of PDVs.  I think we can avoid copying-related
+performance issues if this is THE place where the associated DataSets reside, and
+they are passed to other classes by reference.  After all, I don't see why a
+Command or Data message would ever be changed after it had been received.
 */
+
 namespace dicom
 {
+  namespace MessageControlHeader
+  {
+    typedef std::uint8_t Code;
+    const Code DATASET = 0x00;
+    const Code COMMAND = 0x01;
+    const Code LAST_FRAGMENT = 0x02;
+  }
 
+/*!
+defined in Part8, table 9-22
+Further documentation in Part 8, Annex E
 
-	namespace MessageControlHeader
-	{
-		typedef BYTE Code;
-		const Code	DATASET			= 0x00,
-					COMMAND 		= 0x01,
-					LAST_FRAGMENT	= 0x02;
-	}
+I think this class needs to be seriously re-thought, or even removed altogether,
+as it's implementation is very misleading.  This class does _not_ corresponding
+exactly to the P_DATA_TF request primitive, rather it serves to manage a _stream_
+of them!
 
+We need to know the maximum length specified during association negotiation.
 
+*/
 
-	/*!
-		defined in Part8, table 9-22
-		Further documentation in Part 8, Annex E
+  class PDataTF
+  {
+    static const std::uint8_t m_itemType = 0x04;
+    static const std::uint8_t m_reserved1 = 0x00;
 
-		I think this class needs to be seriously re-thought, or even removed altogether,
-		as it's implementation is very misleading.  This class does _not_ corresponding
-		exactly to the P_DATA_TF request primitive, rather it serves to manage a _stream_
-		of them!
-		
-		We need to know the maximum length specified during association negotiation.
+  public:
 
-		
-	*/
-	class	PDataTF
-	{
-	public:
+    Buffer m_buffer;
 
-		Buffer buffer_;
-		const static BYTE			ItemType = 0x04;
-		const static BYTE			Reserved1 = 0x00;
+    std::uint32_t m_length;
 
+    std::int32_t m_msgStatus;
+    std::uint8_t m_presentationContextID;
+    std::uint8_t m_messageHeader;
 
-		UINT32			Length;
-
-		int		MsgStatus;
-		BYTE	PresentationContextID;
-		BYTE	MessageHeader;
-
-		bool	ReadDynamic(Network::Socket& socket);
-
-				PDataTF(int ByteOrder);
-	};
+    PDataTF(int ByteOrder);
+    
+    bool read(Buffer& temp);
+  };
 }//namespace dicom
 
-#endif //P_DATA_HPP_INCLUDE_GUARD_4520084566
