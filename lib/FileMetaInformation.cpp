16229547552f9b6126dcadd8140fd4ea8cc6b2d4
mechmaster <vladimir-serd@yadex.ru>
2017-10-15 16:20:43 +0400

    Refactoring FileMetaInformation

--- lib/FileMetaInformation.cpp
+++ lib/FileMetaInformation.cpp
@@ -15,203 +15,127 @@
 #include "GroupLength.hpp"
 #include "UIDs.hpp"
 
-//#include "ImageDisplay/TiffFile.hpp"
-
-
 namespace dicom
 {
-// 	//!Figure out the length in bytes of a dataset by writing it to a temporary buffer.
-// 	/*!
-// 		This is probably an inefficient way of doing this, but the
-// 		meta-info will generally be only of the order of a couple
-// 		of hundred bytes at most, so I don't think this will have
-// 		much impact on performance.
-//
-// 		Note that the group length is dependent on which transfer syntax we use, as
-// 		explicit vr writes more info than implicit vr.
-//
-// 		This is actually needed in more places than here...
-// 	*/
-//
-// 	UINT32 GetGroupLength(DataSet& data,TS ts)
-// 	{
-// 		int ByteOrder=ts.isBigEndian()?
-// 			__BIG_ENDIAN:__LITTLE_ENDIAN;
-//
-// 		Buffer buffer(ByteOrder);
-// 		//Encoder E(buffer,dataset,ts);
-// 		//E.Encode();
-// 		WriteToBuffer(data,buffer,ts);
-// 		return (buffer.size());
-// 	}
-
-	void VerifyAcceptable(Tag tag,UID uid)
-	{
-		//verification code goes here.
-	}
+  void VerifyAcceptable(Tag tag, UID uid)
+  {
+    //http://dicom.nema.org/Dicom/2013/output/chtml/part04/sect_B.5.html
+    //http://dicom.nema.org/Dicom/2013/output/chtml/part10/chapter_7.html
+  //verification code goes here.
+    
+    // Написать код проверки на собкласс по списку возможных значений.
+  }
 
 
-	/*!
-		Refer to Part 10, Section 7.1 for a description of the fields here.
-	*/
+  /*!
+  Refer to Part 10, Section 7.1 for a description of the fields here.
+  */
 
-	FileMetaInformation::FileMetaInformation(const DataSet& data,TS ts)
-	{
-		const UID& classUID=data(TAG_SOP_CLASS_UID).Get<UID>();
-		const UID& instUID=data(TAG_SOP_INST_UID).Get<UID>();
-		VerifyAcceptable(TAG_MEDIA_SOP_CLASS_UID,classUID);//table 7.1-1 of part 10 implies some checking should occur here...
+  FileMetaInformation::FileMetaInformation(const DataSet& data, TS ts)
+  {
+    const UID& classUID = data(TAG_SOP_CLASS_UID).Get<UID>();
+    const UID& instUID = data(TAG_SOP_INST_UID).Get<UID>();
+    VerifyAcceptable(TAG_MEDIA_SOP_CLASS_UID,classUID);//table 7.1-1 of part 10 implies some checking should occur here...
 
-		std::fill(Preamble_,Preamble_+128,0);
+    TypeFromVR<VR_OB>::Type VersionInfoData;//Set to (0,1) for the version of the standard that we support.
+    VersionInfoData.push_back(0x00);
+    VersionInfoData.push_back(0x01);
+    m_metaElements.put<VR_OB>(TAG_FILE_INFO_VERS, VersionInfoData);
 
-		TypeFromVR<VR_OB>::Type VersionInfoData;//Set to (0,1) for the version of the standard that we support.
-		VersionInfoData.push_back(0x00);
-		VersionInfoData.push_back(0x01);
-		MetaElements_.put<VR_OB>(TAG_FILE_INFO_VERS,VersionInfoData);
+    m_metaElements.put<VR_UI>(TAG_MEDIA_SOP_CLASS_UID, classUID);
+    m_metaElements.put<VR_UI>(TAG_MEDIA_SOP_INST_UID, instUID);
+    m_metaElements.put<VR_UI>(TAG_TRANSFER_SYNTAX_UID, ts.getUID());
+    m_metaElements.put<VR_UI>(TAG_IMPL_CLASS_UID, UID(ImplementationClassUID));
+    m_metaElements.put<VR_SH>(TAG_IMPL_VERS_NAME, ImplementationVersionName);
 
-		MetaElements_.put<VR_UI>(TAG_MEDIA_SOP_CLASS_UID,classUID);
-		MetaElements_.put<VR_UI>(TAG_MEDIA_SOP_INST_UID,instUID);
-		MetaElements_.put<VR_UI>(TAG_TRANSFER_SYNTAX_UID,ts.getUID());
-		MetaElements_.put<VR_UI>(TAG_IMPL_CLASS_UID,UID(ImplementationClassUID));
-		MetaElements_.put<VR_SH>(TAG_IMPL_VERS_NAME,ImplementationVersionName);
-		//MetaElements_.Put<VR_AE>(TAG_SRC_AET,SourceApplicationTitle);//this is optional
-		UINT32 group_length=GroupLength(MetaElements_,TS(EXPL_VR_LE_TRANSFER_SYNTAX/*TS::EXPL_VR_LE*/));
+    std::uint32_t group_length = GroupLength(m_metaElements, TS(EXPL_VR_LE_TRANSFER_SYNTAX));
 
-		MetaElements_.put<VR_UL> (TAG_FILE_INFO_GR_LEN,group_length);//because multimap is sorted, this will
-																	//get inserted at the front of MetaElements_
+    m_metaElements.put<VR_UL>(TAG_FILE_INFO_GR_LEN, group_length); //because multimap is sorted, this will
+      //get inserted at the front of MetaElements_
 
-		if(MetaElements_.find(TAG_FILE_INFO_GR_LEN)!=MetaElements_.begin())
-			throw exception("TAG_FILE_INFO_GR_LEN not at begin.");//test above assertion!
+    if(m_metaElements.find(TAG_FILE_INFO_GR_LEN) != m_metaElements.begin())
+    {
+      throw exception("TAG_FILE_INFO_GR_LEN not at begin."); //test above assertion!
+    }
+  }
 
-	}
+  FileMetaInformation::FileMetaInformation(std::istream& In)
+  {
+    if (!In)
+    {
+      throw FileMetaInfoException("Input stream not open.");
+    }
 
+    if(!In.seekg(0))
+    {
+      throw FileMetaInfoException("Couldn't find beginning of stream.");
+    }
 
-/*
-	void MakePreampleTiffHeader(const DataSet& data, long BytesToWrite, char* Preamble)
-	{
-		unsigned short width, height, samples_per_px, bits_allocated;
+    if(!In.read(reinterpret_cast<char*>(&m_preamble[0]), 128))
+    {
+      throw FileMetaInfoException("Couldn't read preamble.");
+    }
+    
+    std::string prefix(4, 0);
+    if(!In.read(&prefix[0], prefix.size()))
+    {
+      throw FileMetaInfoException("Couldn't read prefix.");
+    }
 
-		data(dicom::TAG_COLUMNS) >> width;
-		data(dicom::TAG_ROWS) >> height;
-		data(TAG_SAMPLES_PER_PX) >> samples_per_px;
-		data(TAG_BITS_ALLOC) >> bits_allocated;	
+    if (prefix != "DICM")
+    {
+      throw FileMetaInfoException("Prefix is not 'DICM'.");
+    }
 
-		ImageDisplay::TiffFileHeader tiff_header_(width, height, samples_per_px, bits_allocated, BytesToWrite);
+    Buffer buffer(__LITTLE_ENDIAN);//Section 7.1 says this has to be used.
 
-		for(int i = 0; i<128; i++)
-			Preamble[i] = tiff_header_.buf[i];
-	}
+    std::vector<std::uint8_t> temp(12, 0);
+    In.read(reinterpret_cast<char*>(&temp[0]), temp.size());
+    buffer.AddVector(temp);
 
-*/
-	FileMetaInformation::FileMetaInformation(const DataSet& data,TS ts,long BytesToWrite)
-	{
-		const UID& classUID=data(TAG_SOP_CLASS_UID).Get<UID>();
-		const UID& instUID=data(TAG_SOP_INST_UID).Get<UID>();
-		VerifyAcceptable(TAG_MEDIA_SOP_CLASS_UID,classUID);//table 7.1-1 of part 10 implies some checking should occur here...
+    TS ts(EXPL_VR_LE_TRANSFER_SYNTAX); //Section 7.1 says this has to be used.
 
-		std::fill(Preamble_,Preamble_+128,0);
+    ReadElementFromBuffer(buffer, m_metaElements, ts);//gets the 'length' element.
 
-		//Adding Tiff metaheader
-		//MakePreampleTiffHeader(data, BytesToWrite, Preamble_);
+    Tag tag = m_metaElements.begin()->first;
+    if(tag != TAG_FILE_INFO_GR_LEN)
+    {
+      throw exception("First tag must be group length(0x0002,0000) in File Meta Information");
+    }
 
-		TypeFromVR<VR_OB>::Type VersionInfoData;//Set to (0,1) for the version of the standard that we support.
-		VersionInfoData.push_back(0x00);
-		VersionInfoData.push_back(0x01);
-		MetaElements_.put<VR_OB>(TAG_FILE_INFO_VERS,VersionInfoData);
+    Value& value = m_metaElements.begin()->second;
 
-		MetaElements_.put<VR_UI>(TAG_MEDIA_SOP_CLASS_UID,classUID);
-		MetaElements_.put<VR_UI>(TAG_MEDIA_SOP_INST_UID,instUID);
-		MetaElements_.put<VR_UI>(TAG_TRANSFER_SYNTAX_UID,ts.getUID());
-		MetaElements_.put<VR_UI>(TAG_IMPL_CLASS_UID,UID(ImplementationClassUID));
-		MetaElements_.put<VR_SH>(TAG_IMPL_VERS_NAME,ImplementationVersionName);
-		//MetaElements_.Put<VR_AE>(TAG_SRC_AET,SourceApplicationTitle);//this is optional
-		UINT32 group_length=GroupLength(MetaElements_,TS(EXPL_VR_LE_TRANSFER_SYNTAX/*TS::EXPL_VR_LE*/));
+    std::uint32_t FileMetaInfoLength;
+    value >> FileMetaInfoLength;
 
-		MetaElements_.put<VR_UL> (TAG_FILE_INFO_GR_LEN,group_length);//because multimap is sorted, this will
-																	//get inserted at the front of MetaElements_
+    //now read the rest of the file meta info from the input stream
 
-		if(MetaElements_.find(TAG_FILE_INFO_GR_LEN)!=MetaElements_.begin())
-			throw exception("TAG_FILE_INFO_GR_LEN not at begin.");//test above assertion!
+    buffer.clear();
 
-	}
+    temp.resize(FileMetaInfoLength);
+    In.read(reinterpret_cast<char*>(&temp[0]), temp.size());
+    buffer.AddVector(temp);
 
+    //now parse onto meta info set.
+    ReadFromBuffer(buffer, m_metaElements, ts);
 
-	FileMetaInformation::FileMetaInformation(std::istream& In)
-	{
-		if(!In)
-			throw FileMetaInfoException("Input stream not open.");
-		if(!In.seekg(0))
-			throw FileMetaInfoException("Couldn't find beginning of stream.");
-		if(!In.read(Preamble_,128))
-			throw FileMetaInfoException("Couldn't read preamble.");
+    //done, and I should be at the correct point in the file to continue reading!
+  }
 
-		char Prefix[4];
-		if(!In.read(Prefix,4))
-			throw FileMetaInfoException("Couldn't read prefix.");
-		if(std::string(Prefix,Prefix+4) !="DICM")
-			throw FileMetaInfoException("Prefix is not 'DICM'.");
+  std::uint32_t FileMetaInformation::write(std::ostream& Out)
+  {
+    Out.seekp(0);//always go to the beginning
 
+    Out.write(reinterpret_cast<char*>(&m_preamble[0]), m_preamble.size());
 
-		Buffer buffer(__LITTLE_ENDIAN);//Section 7.1 says this has to be used.
+    std::string prefix("DICM");
+    Out.write(prefix.c_str(), prefix.size());
 
-		//we know the length of this element...
-		for(int I=0;I<12;I++)
-			buffer.push_back(In.get());//This seems pretty inefficient...
+    Buffer buffer(__LITTLE_ENDIAN);
 
-		TS ts(EXPL_VR_LE_TRANSFER_SYNTAX/*TS::EXPL_VR_LE*/); //Section 7.1 says this has to be used.
+    WriteToBuffer(m_metaElements, buffer, TS(EXPL_VR_LE_TRANSFER_SYNTAX));//Section 7.1 says this syntax has to be used.
+    Out.write(reinterpret_cast<char*>(&buffer[0]), buffer.size());
 
-		ReadElementFromBuffer(buffer,MetaElements_,ts);//gets the 'length' element.
-
-
-		Tag tag=MetaElements_.begin()->first;
-		if(tag!=TAG_FILE_INFO_GR_LEN)
-			throw exception("First tag must be group length(0x0002,0000) in File Meta Information");
-		Value& value=MetaElements_.begin()->second;
-
-		UINT32 FileMetaInfoLength;
-		value >> FileMetaInfoLength;
-
-		//now read the rest of the file meta info from the input stream
-
-		Buffer buffer2(__LITTLE_ENDIAN);
-
-		for(UINT32 i=0;i<FileMetaInfoLength;i++)
-			buffer2.push_back(In.get());
-
-		//now parse onto meta info set.
-		ReadFromBuffer(buffer2,MetaElements_,ts);
-
-		//done, and I should be at the correct point in the file to continue reading!
-	}
-
-
-	int FileMetaInformation::Write(std::ostream& Out)
-	{
-		Out.seekp(0);//always go to the beginning
-
-		//first the preamble...
-		for(int i=0;i<128;i++)
-			//Out.put(0);
-			Out.put(Preamble_[i]);   
-		
-
-		Out.put('D');
-		Out.put('I');
-		Out.put('C');
-		Out.put('M');
-
-		Buffer buffer(__LITTLE_ENDIAN);
-
-
-		WriteToBuffer(MetaElements_,buffer,TS(EXPL_VR_LE_TRANSFER_SYNTAX/*TS::EXPL_VR_LE*/));//Section 7.1 says this syntax has to be used.
-
-		while(buffer.position()!=buffer.end())
-		{
-			Out.put(*buffer.position());
-			buffer.increment(1);
-		}
-
-		return (buffer.size() + 128 + 4);
-
-	}
-
+    return (buffer.size() + 128 + 4);
+  }
 }//namespace dicom
--- lib/FileMetaInformation.hpp
+++ lib/FileMetaInformation.hpp
@@ -1,54 +1,51 @@
-#ifndef FILE_META_INFORMATION_HPP_INCLUDE_GUARD_784364843843
-#define FILE_META_INFORMATION_HPP_INCLUDE_GUARD_784364843843
+#pragma once
+
 #include <iostream>
+#include <array>
+
 #include "DataSet.hpp"
 #include "Exceptions.hpp"
 #include "TransferSyntax.hpp"
 #include "ImplementationUID.hpp"
 
-
 namespace dicom
 {
+  struct FileMetaInfoException : public dicom::exception
+  {
+    FileMetaInfoException(const std::string& description) : exception(description)
+    {
+    }
+  };
 
-	struct FileMetaInfoException:public dicom::exception
-	{
-		FileMetaInfoException(std::string Description):exception(Description){}
-	};
+  //!DICOM File Meta Information
+  /*!
+  Documented in Part 10, Section 7.1
 
-	//!DICOM File Meta Information
-	/*!
-		Documented in Part 10, Section 7.1
+  Consists of:
+  Preamble (128 bytes)
+  DICOM prefix (4 bytes)
+  File Meta Elements (Part 10, table 7.1-1)
+  */
 
-		Consists of:
-		Preamble (128 bytes)
-		DICOM prefix (4 bytes)
-		File Meta Elements (Part 10, table 7.1-1)
-	*/
-	class FileMetaInformation
-	{
-	public:
-		/*
-			What constructors are we going to allow?
-			-Read _from_ a stream
-			-construct with the intention of writing _to_ a stream.
-		*/
-		FileMetaInformation(std::istream& In);
-		//FileMetaInformation(const UID& classUID, const UID& instUID, TS ts);
+  class FileMetaInformation
+  {
+    std::array<std::uint8_t, 128> m_preamble;
+    
+    DataSet m_metaElements;
 
-		//!Infer instance and class UIDs from data set
-		FileMetaInformation(const DataSet& data,TS ts);
-		
-		//to make a Tiff-Dicom !mge @May 2009
-		FileMetaInformation(const DataSet& data,TS ts,long BytesToWrite);
+  public:
 
-		char Preamble_[128];
-		DataSet MetaElements_;
-		int/*void*/ Write(std::ostream& Out);
+    /*
+    What constructors are we going to allow?
+    -Read _from_ a stream
+    -construct with the intention of writing _to_ a stream.
+    */
 
-	};
+    FileMetaInformation(std::istream& In);
 
+    //!Infer instance and class UIDs from data set
+    FileMetaInformation(const DataSet& data, TS ts);
 
+    std::uint32_t write(std::ostream& Out);
+  };
 }//namespace dicom
-
-
-#endif //FILE_META_INFORMATION_HPP_INCLUDE_GUARD_784364843843
