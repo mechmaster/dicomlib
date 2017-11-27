0dbd8a464bb7309dcd108417c29195d8577da756
mechmaster <vladimir-serd@yadex.ru>
2017-11-27 22:47:05 +0400

    Refactoring QueryRetrieve Refactoring main.cpp

--- lib/QueryRetrieve.cpp
+++ lib/QueryRetrieve.cpp
@@ -1,130 +1,139 @@
-#include "QueryRetrieve.hpp"
+#include <map>
+
 #include "Exceptions.hpp"
+#include "QueryRetrieve.hpp"
 
 namespace dicom
 {
-	namespace QueryRetrieve
-	{
-		Level GetLevel(const std::string& level)
-		{
-			if(level=="PATIENT")
-				return PATIENT;
-			if(level=="STUDY")
-				return STUDY;
-			if(level=="SERIES")
-				return SERIES;
-			if(level=="IMAGE")
-				return IMAGE;
-			throw exception("Unknown Query/Retrieve level specified.");
-		}
-		Level ParentLevel(Level level)
-		{
-			switch (level)
-			{
-				case STUDY:
-					return PATIENT;
-				case SERIES:
-					return STUDY;
-				case IMAGE:
-					return SERIES;
-				default:
-					throw exception("Invalid Query/Retrieve level value.");
-			}
-		}
-		Level Child(Level level)
-		{
-			switch (level)
-			{
-				case PATIENT:
-					return STUDY;
-				case STUDY:
-					return SERIES;
-				case SERIES:
-					return IMAGE;
-				default:
-					throw exception("Invalied Query/Retrieve level value.");
-			}
-		}
+  namespace QueryRetrieve
+  {
+    Level GetLevel(const std::string& level)
+    {
+      static std::map<std::string, Level> stingToLevelList = 
+      {
+        {"PATIENT", Level::PATIENT},
+        {"STUDY", Level::STUDY},
+        {"SERIES", Level::SERIES},
+        {"IMAGE", Level::IMAGE},
+      };
+      
+      auto iter = stingToLevelList.find(level);
+      if (iter == stingToLevelList.end())
+      {
+        throw exception("Unknown Query/Retrieve level specified.");
+      }
+      
+      return iter->second;
+    }
 
-		std::string ToString(QueryRetrieve::Level level)
-		{
-			using namespace QueryRetrieve;
-			switch (level)
-			{
-				case PATIENT:
-					return "PATIENT";
-				case STUDY:
-					return "STUDY";
-				case SERIES:
-					return "SERIES";
-				case IMAGE:
-					return "IMAGE";
-				default:
-					throw exception("Unknown Query/Retrieve level value.");
-			}
-		}
+    Level ParentLevel(Level level)
+    {
+      switch (level)
+      {
+      case Level::STUDY:
+        return Level::PATIENT;
+      case Level::SERIES:
+        return Level::STUDY;
+      case Level::IMAGE:
+        return Level::SERIES;
+      default:
+        throw exception("Invalid Query/Retrieve level value.");
+      }
+    }
 
+    Level Child(Level level)
+    {
+      switch (level)
+      {
+      case Level::PATIENT:
+        return Level::STUDY;
+      case Level::STUDY:
+        return Level::SERIES;
+      case Level::SERIES:
+        return Level::IMAGE;
+      default:
+        throw exception("Invalied Query/Retrieve level value.");
+      }
+    }
 
-		/*!
-			Are we sure about this list? Is it dependent
-			on QR model?
-		*/
+    std::string ToString(Level level)
+    {
+      switch (level)
+      {
+      case Level::PATIENT:
+        return "PATIENT";
+      case Level::STUDY:
+        return "STUDY";
+      case Level::SERIES:
+        return "SERIES";
+      case Level::IMAGE:
+        return "IMAGE";
+      default:
+        throw exception("Unknown Query/Retrieve level value.");
+      }
+    }
 
-		Level GetLevel(Tag tag)
-		{
-			switch (tag)
-			{
-				case TAG_PAT_NAME:
-				case TAG_PAT_ID:
-				case TAG_PAT_BIRTH_DATE:
-				case TAG_PAT_SEX:
-				case TAG_OTHER_PAT_ID:
-				case TAG_PAT_COMMENT:
-					return PATIENT;
-				case TAG_STUDY_DATE:
-				case TAG_STUDY_TIME:
-				case TAG_ACCESS_NO:
-				case TAG_STUDY_ID:
-				case TAG_STUDY_INST_UID:
-				case TAG_REF_PHYS_NAME:
-				case TAG_STUDY_DESC:
-				case TAG_READ_PHYS_NAME:
-				case TAG_ADMIT_DIAG_DESC:
-				case TAG_PAT_AGE:
-				case TAG_PAT_SIZE:
-				case TAG_PAT_WEIGHT:
-				case TAG_INTERPRET_AUTHOR:
-					return STUDY;
-				case TAG_MODALITY:
-				case TAG_SERIES_NO:
-				case TAG_SERIES_INST_UID:
-					return SERIES;
-				default:
-					return IMAGE;
-			}
-		}
-		Tag GetUIDTag(const Level level)
-		{
-			switch (level)
-			{
-				case PATIENT:
-					return TAG_PAT_ID;
-				case STUDY:
-					return TAG_STUDY_INST_UID;
-				case SERIES:
-					return TAG_SERIES_INST_UID;
-				case IMAGE:
-					return TAG_SOP_INST_UID;
-				default:
-					throw exception("Invalid Query/Retrieve level value.");
-			}
-		}
-	}//namespace QueryRetrieve
+    /*!
+    Are we sure about this list? Is it dependent
+    on QR model?
+    */
 
-	//!should this be in the global namespace?
-	std::ostream& operator <<  (std::ostream& out, QueryRetrieve::Level level)
-	{
-		out << QueryRetrieve::ToString(level);
-		return out;
-	}
+    Level GetLevel(Tag tag)
+    {
+      switch (tag)
+      {
+      case TAG_PAT_NAME:
+      case TAG_PAT_ID:
+      case TAG_PAT_BIRTH_DATE:
+      case TAG_PAT_SEX:
+      case TAG_OTHER_PAT_ID:
+      case TAG_PAT_COMMENT:
+        return Level::PATIENT;
+      case TAG_STUDY_DATE:
+      case TAG_STUDY_TIME:
+      case TAG_ACCESS_NO:
+      case TAG_STUDY_ID:
+      case TAG_STUDY_INST_UID:
+      case TAG_REF_PHYS_NAME:
+      case TAG_STUDY_DESC:
+      case TAG_READ_PHYS_NAME:
+      case TAG_ADMIT_DIAG_DESC:
+      case TAG_PAT_AGE:
+      case TAG_PAT_SIZE:
+      case TAG_PAT_WEIGHT:
+      case TAG_INTERPRET_AUTHOR:
+        return Level::STUDY;
+      case TAG_MODALITY:
+      case TAG_SERIES_NO:
+      case TAG_SERIES_INST_UID:
+        return Level::SERIES;
+      default:
+        return Level::IMAGE;
+      }
+    }
+
+    Tag GetUIDTag(const Level level)
+    {
+      switch (level)
+      {
+      case Level::PATIENT:
+        return Level::TAG_PAT_ID;
+      case Level::STUDY:
+        return Level::TAG_STUDY_INST_UID;
+      case Level::SERIES:
+        return Level::TAG_SERIES_INST_UID;
+      case Level::IMAGE:
+        return Level::TAG_SOP_INST_UID;
+      default:
+        throw exception("Invalid Query/Retrieve level value.");
+      }
+    }
+  }//namespace QueryRetrieve
+
+  //!should this be in the global namespace?
+  std::ostream& operator << (std::ostream& out, QueryRetrieve::Level level)
+  {
+    out << QueryRetrieve::ToString(level);
+    return out;
+  }
 }//namespace dicom
--- main.cpp
+++ main.cpp
@@ -14,162 +14,167 @@
 //#include <FL/Fl.H>
 
 /*
-	modify these constants as appropriate for your environment
+modify these constants as appropriate for your environment
 */
 
 const std::string host = "192.168.3.148";
 const std::string local_AE = "mechgrad";
 const std::string remote_AE = "ORTHANC";
-const short remote_port = 4242;		//standard dicom port
+const short remote_port = 4242; //standard dicom port
 
 unsigned int count = 0;
 std::chrono::duration<double> elapsed_seconds;
 
 namespace demo
 {
-	void OpenDataSet(std::string filename)
-	{
-		dicom::DataSet data;
-        
-        try
-        {
-          dicom::Read(filename, data);
-        }
-        catch(...)
-        {
-          return;
-        }
+  void OpenDataSet(std::string filename)
+  {
+    dicom::DataSet data;
 
-        ++count;
+    try
+    {
+      dicom::Read(filename, data);
+    }
+    catch(...)
+    {
+      return;
+    }
 
-        std::cout << "File Opened" << std::endl;
-		std::cout << data << std::endl;
-        
-		/*std::string PatientID;
-        std::string PatientBD;
-        std::string SeriesNO;
-        std::string Modality;
-        std::string PatientName;
-        std::string ImagePosition;
+    ++count;
 
-		data(dicom::TAG_PAT_ID) >> PatientID;
-        data(dicom::TAG_PAT_BIRTH_DATE) >> PatientBD;
-        data(dicom::TAG_PAT_NAME) >> PatientName;
-        data(dicom::TAG_SERIES_NO) >> SeriesNO;
-        data(dicom::TAG_MODALITY) >> Modality;
+    std::cout << "File Opened" << std::endl;
+    std::cout << data << std::endl;
 
-		std::cout << "Patient ID is " << PatientID << std::endl;
-        std::cout << "Patient Birth Date is " << PatientBD << std::endl;
-        std::cout << "Patient name is " << PatientName << std::endl;
-        std::cout << "Series NO = " << SeriesNO << std::endl;
-        std::cout << "Modality = " << Modality << std::endl;*/
-		
-		//We can access underlying pixel data as follows:
-		std::vector<unsigned short> PixelData;
+    /*std::string PatientID;
+    std::string PatientBD;
+    std::string SeriesNO;
+    std::string Modality;
+    std::string PatientName;
+    std::string ImagePosition;
 
-		data(dicom::TAG_PIXEL_DATA) >> PixelData;
-	}
+    data(dicom::TAG_PAT_ID) >> PatientID;
+    data(dicom::TAG_PAT_BIRTH_DATE) >> PatientBD;
+    data(dicom::TAG_PAT_NAME) >> PatientName;
+    data(dicom::TAG_SERIES_NO) >> SeriesNO;
+    data(dicom::TAG_MODALITY) >> Modality;
 
-	void ConnectToRemoteServer()
-	{
-		dicom::PresentationContexts presentation_contexts;
-		presentation_contexts.Add(dicom::MAMMO_PRES_IMAGE_STORAGE_SOP_CLASS);
-		presentation_contexts.Add(dicom::MAMMO_PROC_IMAGE_STORAGE_SOP_CLASS);
-		//add further presentation contexts as appropriate...
-		dicom::ClientConnection connection(host, remote_port, local_AE, remote_AE, presentation_contexts);
-	}
+    std::cout << "Patient ID is " << PatientID << std::endl;
+    std::cout << "Patient Birth Date is " << PatientBD << std::endl;
+    std::cout << "Patient name is " << PatientName << std::endl;
+    std::cout << "Series NO = " << SeriesNO << std::endl;
+    std::cout << "Modality = " << Modality << std::endl;*/
 
-	void PrintResult(const dicom::DataSet& data)
-	{
-		std::cout << "Query result:" << std::endl << data << std::endl;
-	}
-	void QueryRemoteServer()
-	{
-		dicom::PresentationContexts presentation_contexts;
-		presentation_contexts.Add(dicom::STUDY_ROOT_QR_FIND_SOP_CLASS);
-	
-		dicom::ClientConnection connection(host, remote_port, local_AE, remote_AE, presentation_contexts);
-		
-		
-		//build a query to search for all studies on patients names beginning with "A"
-		dicom::DataSet query;
-		query.put<dicom::VR_CS>(dicom::TAG_QR_LEVEL,std::string("STUDY"));
-		query.put<dicom::VR_UI>(dicom::TAG_STUDY_INST_UID,dicom::UID("*"));
-		query.put<dicom::VR_PN>(dicom::TAG_PAT_NAME,std::string("*"));
-		
-		//execute the query
-		std::vector<dicom::DataSet> result = connection.find(query,dicom::QueryRetrieve::STUDY_ROOT);
+    //We can access underlying pixel data as follows:
+    std::vector<unsigned short> PixelData;
 
-		//print the results
-		std::for_each(result.begin(),result.end(),PrintResult);
-	}
+    data(dicom::TAG_PIXEL_DATA) >> PixelData;
+  }
 
-	/*!
-		Instruct the pacs server to send the set of images belonging to
-        a given study to another DICOM application entity
-	*/
-	void DoCMove()
-	{
-		dicom::PresentationContexts presentation_contexts;
-		presentation_contexts.Add(dicom::STUDY_ROOT_QR_MOVE_SOP_CLASS);
-		//presentation_contexts.Add(dicom::PATIENT_ROOT_QR_MOVE_SOP_CLASS);
+  void ConnectToRemoteServer()
+  {
+    dicom::PresentationContexts presentation_contexts;
+    presentation_contexts.Add(dicom::MAMMO_PRES_IMAGE_STORAGE_SOP_CLASS);
+    presentation_contexts.Add(dicom::MAMMO_PROC_IMAGE_STORAGE_SOP_CLASS);
 
-		dicom::ClientConnection connection(host, remote_port, local_AE, remote_AE, presentation_contexts);
-		
-        static unsigned int count = 0;
-        static std::vector<std::string> UID_list = boost::assign::list_of ("1.2.392.200036.9116.2.6.1.48.1214242831.1408519596.617924")
-        ("1.2.392.200036.9116.2.6.1.48.1214242831.1408519682.959652")
-        ("1.2.392.200036.9116.2.6.1.48.1214242831.1408519696.253333")
-        ("1.2.392.200036.9116.2.6.1.48.1214242831.1408520084.921691")
-        ("1.2.392.200036.9116.2.6.1.48.1214242831.1408520181.188813");
-        
-		//make CMove request
-		dicom::DataSet request;
-		request.put<dicom::VR_CS>(dicom::TAG_QR_LEVEL,std::string("SERIES"));
-        
-        ++count;
-        if (count >= UID_list.size())
-        {
-          count = 0;
-        }
-		request.put<dicom::VR_UI>(dicom::TAG_SERIES_INST_UID,dicom::UID(UID_list[count]));
-        
-        std::string  serverWorkingDirectory("/home/chif/.local/share/SamSMU/pacs/dicom/");
-        boost::filesystem::path path(serverWorkingDirectory);
-        if (boost::filesystem::exists(path)) {
-          std::vector<std::string> deletePathList;
+    //add further presentation contexts as appropriate...
+    dicom::ClientConnection connection(host, remote_port, local_AE, remote_AE, presentation_contexts);
+  }
 
-          boost::filesystem::recursive_directory_iterator directoryIterator(serverWorkingDirectory), endIterator;
-          while (directoryIterator != endIterator) {
-            boost::filesystem::path path = directoryIterator->path();
+  void PrintResult(const dicom::DataSet& data)
+  {
+    std::cout << "Query result:" << std::endl << data << std::endl;
+  }
 
-            deletePathList.push_back(path.string());
+  void QueryRemoteServer()
+  {
+    dicom::PresentationContexts presentation_contexts;
+    presentation_contexts.Add(dicom::STUDY_ROOT_QR_FIND_SOP_CLASS);
 
-            ++directoryIterator;
-          }
+    dicom::ClientConnection connection(host, remote_port, local_AE, remote_AE, presentation_contexts);
 
-          for (unsigned int i = 0; i < deletePathList.size(); ++i) {
-            boost::filesystem::remove(deletePathList[i]);
-          }
-        }
-        else {
-          std::cout << boost::str(boost::format("Fatal error! path=%s not exists.") % serverWorkingDirectory) << std::endl;
+    //build a query to search for all studies on patients names beginning with "A"
+    dicom::DataSet query;
+    query.put<dicom::VR_CS>(dicom::TAG_QR_LEVEL, std::string("STUDY"));
+    query.put<dicom::VR_UI>(dicom::TAG_STUDY_INST_UID, dicom::UID("*"));
+    query.put<dicom::VR_PN>(dicom::TAG_PAT_NAME, std::string("*"));
 
-          return;
-        }
+    //execute the query
+    std::vector<dicom::DataSet> result = connection.find(query, dicom::QueryRetrieve::Root::STUDY_ROOT);
 
-        try
-        {
-          //request that a set of images be sent.
-          dicom::DataSet result=connection.move("MECHGRAD", request, dicom::QueryRetrieve::STUDY_ROOT);
-          std::cout << result << std::endl;
-        }
-        catch(std::exception& e)
-        {
-          std::string msg = e.what();
-          std::cout << msg << std::endl;
-        }
-	}
+    //print the results
+    std::for_each(result.begin(), result.end(), PrintResult);
+  }
+
+  /*!
+  Instruct the pacs server to send the set of images belonging to
+  a given study to another DICOM application entity
+  */
+  void DoCMove()
+  {
+    dicom::PresentationContexts presentation_contexts;
+    presentation_contexts.Add(dicom::STUDY_ROOT_QR_MOVE_SOP_CLASS);
+    //presentation_contexts.Add(dicom::PATIENT_ROOT_QR_MOVE_SOP_CLASS);
+
+    dicom::ClientConnection connection(host, remote_port, local_AE, remote_AE, presentation_contexts);
+
+    static unsigned int count = 0;
+    static std::vector<std::string> UID_list = boost::assign::list_of ("1.2.392.200036.9116.2.6.1.48.1214242831.1408519596.617924")
+    ("1.2.392.200036.9116.2.6.1.48.1214242831.1408519682.959652")
+    ("1.2.392.200036.9116.2.6.1.48.1214242831.1408519696.253333")
+    ("1.2.392.200036.9116.2.6.1.48.1214242831.1408520084.921691")
+    ("1.2.392.200036.9116.2.6.1.48.1214242831.1408520181.188813");
+
+    //make CMove request
+    dicom::DataSet request;
+    request.put<dicom::VR_CS>(dicom::TAG_QR_LEVEL, std::string("SERIES"));
+
+    ++count;
+    if (count >= UID_list.size())
+    {
+      count = 0;
+    }
+    request.put<dicom::VR_UI>(dicom::TAG_SERIES_INST_UID, dicom::UID(UID_list[count]));
+
+    std::string  serverWorkingDirectory("/home/chif/.local/share/SamSMU/pacs/dicom/");
+    boost::filesystem::path path(serverWorkingDirectory);
+    if (boost::filesystem::exists(path))
+    {
+      std::vector<std::string> deletePathList;
+
+      boost::filesystem::recursive_directory_iterator directoryIterator(serverWorkingDirectory), endIterator;
+      while (directoryIterator != endIterator)
+      {
+        boost::filesystem::path path = directoryIterator->path();
+
+        deletePathList.push_back(path.string());
+
+        ++directoryIterator;
+      }
+
+      for (unsigned int i = 0; i < deletePathList.size(); ++i)
+      {
+        boost::filesystem::remove(deletePathList[i]);
+      }
+    }
+    else
+    {
+      std::cout << boost::str(boost::format("Fatal error! path=%s not exists.") % serverWorkingDirectory) << std::endl;
+
+      return;
+    }
+
+    try
+    {
+      //request that a set of images be sent.
+      dicom::DataSet result=connection.move("MECHGRAD", request, dicom::QueryRetrieve::Root::STUDY_ROOT);
+      std::cout << result << std::endl;
+    }
+    catch(std::exception& e)
+    {
+      std::string msg = e.what();
+      std::cout << msg << std::endl;
+    }
+  }
 }//namespace demo
 
 int main(int, char**)
@@ -177,69 +182,68 @@ int main(int, char**)
   //Fl::scheme("gtk+");
   //Fl::lock();
   //Fl::run();
-	//const std::string dicomDir="/home/chif/Patients/";			//modify this as appropriate
+  //const std::string dicomDir="/home/chif/Patients/"; /modify this as appropriate
 
   const std::string dicomDir="/home/chif/test_dicom/91aded844f204d45b098d88f15055bd6/";
   //const std::string dicomDir="/home/chif/test_patients/0003_ZVEREV/dicom/ZVEREV_V_N_20_08_14/DICOM/ST00001/SE00007/";
-  
-    std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
-    startTime = std::chrono::system_clock::now();
-    
-	try
-	{
 
-        std::cout << "*****************************" << std::endl 
-        	      << "* dicomlib demo application *" << std::endl
-        	      << "*****************************" << std::endl;
+  std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
+  startTime = std::chrono::system_clock::now();
 
-        std::cout << std::endl << "Attempting to open a data set from " << dicomDir << std::endl;
-        
-        boost::filesystem::recursive_directory_iterator dir(dicomDir), end;
-        
-        while (dir != end)
+  try
+  {
+    std::cout << "*****************************" << std::endl 
+    << "* dicomlib demo application *" << std::endl
+    << "*****************************" << std::endl;
+
+    std::cout << std::endl << "Attempting to open a data set from " << dicomDir << std::endl;
+
+    boost::filesystem::recursive_directory_iterator dir(dicomDir), end;
+
+    while (dir != end)
+    {
+      boost::filesystem::path path = dir->path();
+
+      std::cout << "path = " << path.string() << std::endl;
+
+      if (boost::filesystem::is_regular_file(path))
+      {
+        try
         {
-          boost::filesystem::path path = dir->path();
-          
-          std::cout << "path = " << path.string() << std::endl;
-          
-          if (boost::filesystem::is_regular_file(path))
-          {
-            try
-            {
-              demo::OpenDataSet(dir->path().string());
-            }
-            catch (...)
-            {
-            }
-          }
-          
-          ++dir;
+          demo::OpenDataSet(dir->path().string());
         }
-
-        std::cout << std::endl << "Attempting to connect to the remote server at " << host << ":" << remote_port << std::endl;
-		//demo::ConnectToRemoteServer();
-
-        std::cout <<  std::endl << "Attempting to query the remote server..." << std::endl;
-		//demo::QueryRemoteServer();
-      
-        /*for (unsigned int i = 0; i < 100; ++i)
+        catch (...)
         {
-          std::cout <<  std::endl << "Attempting to move DICOM..." << std::endl;
-          demo::DoCMove();
-        }*/
-	}
-	catch(std::exception& e)
-	{
-      std::string msg = e.what();
-      std::cout << msg << std::endl;
-	}
-        
-    std::cout << "file count = " << count << std::endl;
-    
-    endTime = std::chrono::system_clock::now();
-    elapsed_seconds = endTime - startTime;
-    
-    std::cout << "elapsed time: " << elapsed_seconds.count() << std::endl;
-  
-	return 0;
+        }
+      }
+
+      ++dir;
+    }
+
+    std::cout << std::endl << "Attempting to connect to the remote server at " << host << ":" << remote_port << std::endl;
+    //demo::ConnectToRemoteServer();
+
+    std::cout <<  std::endl << "Attempting to query the remote server..." << std::endl;
+    //demo::QueryRemoteServer();
+
+    /*for (unsigned int i = 0; i < 100; ++i)
+    {
+      std::cout <<  std::endl << "Attempting to move DICOM..." << std::endl;
+      demo::DoCMove();
+    }*/
+  }
+  catch(std::exception& e)
+  {
+    std::string msg = e.what();
+    std::cout << msg << std::endl;
+  }
+
+  std::cout << "file count = " << count << std::endl;
+
+  endTime = std::chrono::system_clock::now();
+  elapsed_seconds = endTime - startTime;
+
+  std::cout << "elapsed time: " << elapsed_seconds.count() << std::endl;
+
+  return 0;
 }
