90a4034492d3f25570a8be5b62c779173298b282
mechmaster <vladimir-serd@yadex.ru>
2018-01-19 15:20:48 +0400

    Refactoring Tag.

--- lib/Tag.cpp
+++ lib/Tag.cpp
@@ -1,20 +1,22 @@
 /************************************************************************
-*	DICOMLIB
-*	Copyright 2003 Sunnybrook and Women's College Health Science Center
-*	Implemented by Trevor Morgan  (morgan@sten.sunnybrook.utoronto.ca)
+* DICOMLIB
+* Copyright 2003 Sunnybrook and Women's College Health Science Center
+* Implemented by Trevor Morgan  (morgan@sten.sunnybrook.utoronto.ca)
 *
-*	See LICENSE.txt for copyright and licensing info.
+* See LICENSE.txt for copyright and licensing info.
 *************************************************************************/
 
-#include "Tag.hpp"
 #include <sstream>
+
+#include "Tag.hpp"
 #include "DataDictionary.hpp"
+
 namespace dicom
 {
-	 
-    const char* TagNotFound::what() const throw(){
-    	std::ostringstream os;
-    	os << "Tag Not Found: " << GetName(tag_);
-    	return os.str().c_str();
-    }
+  const char* TagNotFound::what() const noexcept
+  {
+    std::ostringstream os;
+    os << "Tag Not Found: " << GetName(m_tag);
+    return os.str().c_str();
+  }
 }
